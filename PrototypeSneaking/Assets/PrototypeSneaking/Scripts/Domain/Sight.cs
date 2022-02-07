5bb647e07606e4efb2e51491e9b84b7ba204d688
shimomuh <shimomuh0501@gmail.com>
2022-02-08 00:50:52 +0900

    add sight feature

---
+++ PrototypeSneaking/Assets/PrototypeSneaking/Scripts/Domain/Sight.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PrototypeSneaking.Domain
+{
+    public class Sight : MonoBehaviour
+    {
+        /// <summary>
+        /// 視野に捉えている GameObject
+        /// </summary>
+        public List<GameObject> CapturedObjects;
+
+        /// <summary>
+        /// 視野に何かを捉えている
+        /// </summary>
+        public bool IsFindingSomething => CapturedObjects.Count > 0;
+        private uint foundCounter = 0;
+        private uint lostCounter = 0;
+
+        /// <summary>
+        /// 何かに気づいたアニメーションをするときに使う
+        /// </summary>
+        public uint GetFoundCountAndReset()
+        {
+            var cnt = foundCounter;
+            foundCounter = 0;
+            return cnt;
+        }
+
+        /// <summary>
+        /// 何かを見失ったアニメーションをするときに使う
+        /// </summary>
+        public uint GetLostCountAndReset()
+        {
+            var cnt = lostCounter;
+            lostCounter = 0;
+            return cnt;
+        }
+
+        public void Awake()
+        {
+            CapturedObjects = new List<GameObject>();
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            foundCounter++;
+            Capture(other.gameObject);
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            lostCounter++;
+            Lose(other.gameObject);
+        }
+
+        private void Capture(GameObject gameObj)
+        {
+            if (CapturedObjects.Exists(obj => obj.GetInstanceID() == gameObj.GetInstanceID())) { return; }
+            CapturedObjects.Add(gameObj);
+        }
+
+        private void Lose(GameObject gameObj)
+        {
+            var index = CapturedObjects.FindIndex(obj => obj.GetInstanceID() == gameObj.GetInstanceID());
+            if (index == -1) { return; }
+            CapturedObjects.RemoveAt(index);
+        }
+    }
+}
---
+++ PrototypeSneaking/Assets/PrototypeSneaking/Scripts/UI/Stage/Sample.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace PrototypeSneaking.UI.Stage
+{
+    public class Sample : MonoBehaviour
+    {
+        [SerializeField] private GameObject button;
+        void Awake()
+        {
+            AddEventTrigger(button, OnClick);
+        }
+
+        public void AddEventTrigger(GameObject gameObj, Action action)
+        {
+            EventTrigger currentTrigger = gameObj.AddComponent<EventTrigger>();
+            currentTrigger.triggers = new List<EventTrigger.Entry>();
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.PointerClick;
+            entry.callback.AddListener((x) => action());
+            currentTrigger.triggers.Add(entry);
+        }
+
+        public void OnClick()
+        {
+        }
+    }
+}
